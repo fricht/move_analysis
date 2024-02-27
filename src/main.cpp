2f97a3951540021b521c254440d35d4ac860b17a
Gedeon.D <gedeon@deveaux.fr>
2024-02-27 23:09:38 +0100

    update *fuck, this wole time i was commiting on main*

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,25 @@
 #include <opencv4/opencv2/opencv.hpp>
+#include <iostream>
 
 
-int main(int argc, char** argv) {}
+int main(int argc, char * argv[]) {
+    if (argc < 2) {
+        std::cout << "missing input file" << std::endl;
+        std::cout << "usage : " << argv[0] << " <path/to/video>" << std::endl;
+        return EXIT_FAILURE;
+    }
+    // flags
+    std::string filepath;
+    bool preview = true;
+    int frame_shift = 10;
+    // TODO : analyse flags
+    filepath = std::string(argv[1]);
+
+    cv::VideoCapture video(filepath);
+    cv::VideoCapture back_video(filepath);
+    if (!video.isOpened() || !back_video.isOpened()) {
+        std::cout << "Error reading " << filepath << std::endl;
+        return EXIT_FAILURE;
+    }
+    //
+}
